87cda21f631fbc86fdf107142b989752034dc514
@avgr2 <arthur.vigier@ipsa.fr>
2026-04-10 09:17:54 +0200

    change in initial conditions

--- mission/forms.py
+++ mission/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class TrajectoryForm(forms.Form):
-    z_p = forms.FloatField(label="Altitude (km)")
-    delta_v = forms.FloatField(label="Delta V (km/s)")
+    z_p = forms.FloatField(label="Altitude (km)", initial=200)
+    delta_v = forms.FloatField(label="Delta V (km/s)", initial=7.17)
     angle_deg = forms.FloatField(label="Angle (deg)", initial=16.7)
